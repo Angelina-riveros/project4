48bea024fe54d316defb1db7734785ed2f765587
Angelina-riveros <angelinariv24@gmail.com>
2025-04-16 01:24:19 -0400

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,56 @@
+publc class Queue<T> implements QueueInterface<T>{
+    private Node firstNode;
+    private Node lastNode;
+
+    public class Node{
+      private T data;
+      private Node next;
+      private Node previous;
+
+      private Node(T dataPortion){
+        data = dataPortion;
+        next = null;
+        previous = null;
+      } 
+    }
+    public Queue(){
+      firstNode = null;
+      lastNode = null;
+    }
+    public void enqueue(T newEntry){
+      Node newNode = new Node(newEntry, null);
+      if(isEmpty()){
+        firstNode = newNode;
+      }else{
+        lastNode.next = newNode;
+        newNode.previous = lastNode;
+      }
+      lastNode = newNode;
+    }
+    public T dequeue(){
+      if(isEmpty()){
+        throw new EmptyQueueException();
+      }
+      T firstOne = getFront();
+      firstNode = firstNode.next;
+      if(firstNode == null){
+        lastNode = null;
+      }else{
+        firstNode.previous(null);
+      }
+      return firstOne;
+    }
+    public T getFront(){
+      if(isEmpty()){
+        throw new EmptyQueueException();
+      }
+      return firstNode.getData();
+    }
+    public boolean isEmpty(){
+      return (firstNode == null) && (lastNode == null);
+    }
+    public void clear(){
+      firstNode = null;
+      lastNode = null;
+    }
+}
