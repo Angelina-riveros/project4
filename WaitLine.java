bfd954256c40e7c7ed8a03681f001724c56c7ad6
Angelina-riveros <angelinariv24@gmail.com>
2025-04-16 01:24:53 -0400

    Create WaitLine.java

---
+++ WaitLine.java
@@ -0,0 +1,34 @@
+public class WaitLine implements WaitLineInterface{
+  private int arrivalTotal;
+  private int servedTotal;
+  private int waitTotal;
+
+  public WaitLine(){
+    arrivalTotal = 0;
+    servedTotal = 0;
+    waitTotal = 0;
+    super();
+  }
+  public void addCustomer(Customer newCustomer){
+    arrivalTotal++;
+    enqueue(newCustomer);
+  }
+  public boolean hasCustomer(){
+    return !isEmpty();
+  }
+  public Customer serveCustomer(int time){
+    Customer served = dequeue();
+    servedTotal++;
+    waitTotal += (time - served.getArrivalTime());
+    return served;
+  }
+  public int getTotalNumArrivals(){
+    return arrivalTotal;
+  }
+  public int getTotalCustomersServed(){
+    return servedTotal;
+  }
+  public int getTotalTimeWaited(){
+    return waitTotal;
+  }
+}
